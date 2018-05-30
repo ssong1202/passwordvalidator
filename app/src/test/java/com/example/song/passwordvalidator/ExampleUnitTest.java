ce1065208404642dcc3a901bc55581a776137211
shauna <sh508672@dal.ca>
2018-05-29 23:30:53 -0300

    add 3 addition rules

--- app/src/test/java/com/example/song/passwordvalidator/ExampleUnitTest.java
+++ app/src/test/java/com/example/song/passwordvalidator/ExampleUnitTest.java
@@ -5,9 +5,9 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
- * Example local unit test, which will execute on the development machine (host).
+ * Shengnan Song
+ *B00735789
  *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
     @Test
